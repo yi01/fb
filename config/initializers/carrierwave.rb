ab530b287ccb0a9dc02c423cef6d1df509f1f998
yuhei ikuzawa <ikuzawayuhei@ikuzawayuutaira-no-MacBook-Air.local>
2017-09-29 15:53:01 +0900

    carrier

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,10 +12,10 @@ CarrierWave.configure do |config|
 
   case Rails.env
     when 'production'
-      config.fog_directory = '本番環境用のバケット名'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/本番環境用のバケット名'
+      config.fog_directory = 'fab1-production'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/fab1-production'
     when 'development'
-      config.fog_directory = '開発環境用のバケット名'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/開発環境用のバケット名'
+      config.fog_directory = 'fab1-development'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/fab1-development'
   end
 end
