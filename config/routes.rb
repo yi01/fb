6734b57e049128df639b95b36480a0255664e8de
yuhei ikuzawa <ikuzawayuhei@ikuzawayuutaira-no-MacBook-Air.local>
2017-09-29 16:03:30 +0900

    aaa

--- config/routes.rb
+++ config/routes.rb
@@ -4,17 +4,19 @@ Rails.application.routes.draw do
     resources :messages
   end
 
-  resources :users, only: [:index]
-  resources :relationships, only: [:create, :destroy]
   devise_for :users, controllers: {
       registrations: "users/registrations",
       omniauth_callbacks: "users/omniauth_callbacks"
   }
+
+  resources :users, only: [:index]
+  resources :relationships, only: [:create, :destroy]
+
   root 'topics#index'
   resources :topics do
     resources :comments
   end
-  if Rails.env.development?
-    mount LetterOpenerWeb::Engine, at: "/letter_opener"
-  end
+  # if Rails.env.development?
+  #   mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  # end
 end
