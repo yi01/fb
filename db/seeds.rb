ed758c4ae2ed2899fb8425de35fc5e8636068e15
yuhei ikuzawa <ikuzawayuhei@ikuzawayuutaira-no-MacBook-Air.local>
2017-09-30 11:07:15 +0900

    image

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,22 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-100.times do |n|
+10.times do |n|
   email = Faker::Internet.email
   provider = Faker::Lorem.word
   uid = Faker::Number.between(1, 1000 )
   password = "password"
+  image_url= Faker::Avatar.image
   avatar = Faker::Avatar.image
   User.create!(email: email,
                password: password,
                password_confirmation: password,
                provider: provider,
                uid: uid,
+               image_url: image_url,
                avatar: avatar
                )
 end
 
-100.times do |n|
+10.times do |n|
   content = Faker::Lorem.word
   Topic.create!(content: content,
 
